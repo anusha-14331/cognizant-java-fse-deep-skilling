3a0e74d45881ff8a799d19eaf44e3cb353d9a240
anusha-14331 <anusayeligar683@gmail.com>
2026-06-13 13:12:17 +0530

    Updated Module 1 Design Patterns hands-ons

---
+++ Module1/FactoryMethodPatternExample/Document.java
@@ -0,0 +1,4 @@
+package Module1.FactoryMethodPatternExample;
+public interface Document {
+    void open();
+}
---
+++ Module1/FactoryMethodPatternExample/DocumentFactory.java
@@ -0,0 +1,6 @@
+package Module1.FactoryMethodPatternExample;
+
+public abstract class DocumentFactory {
+
+    public abstract Document createDocument();
+}
